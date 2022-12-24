d975abd9fd606ab5336404753ba22e9a7f904522
Jacob Chvatal <jake@isnt.online>
2022-12-24 15:01:30 -0500

    the girl reading this

--- pages/bios.js
+++ pages/bios.js
@@ -1,15 +1,35 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Bios.module.css'
-import TopMenu from '../components/TopMenu'
+import styles from '../styles/Bios.module.css';
 
-function Bios() {
-  return (
-    <div className={styles.menu}>
-        <img src="https://i.pinimg.com/originals/b6/fe/0c/b6fe0c5fd3c211e729aabeca0597fa0e.gif" className={styles.background}>
-        <div> Tea Brewing Setup Utility - Copyleft 1984-1999 tmg GbR </div>
+const Bios = () => (
+  <div className={styles.menu}>
+    <img src="https://i.pinimg.com/originals/b6/fe/0c/b6fe0c5fd3c211e729aabeca0597fa0e.gif" className={styles.background}/>
+    <div className={styles.title}>
+      Tea Brewing Setup Utility - Copyleft 1984-1999 tmg GbR
     </div>
-  );
-}
+    <div className={styles.menuBorder}>
+        <div className={styles.menuBody}>
+          Hey
+        </div>
+        <div className={styles.menuBody}>
+          The girl reading this
+        </div>
+        <wh className={styles.menuFooter1}>
+            <div style={{ width: '50%'}}>
+                Esc : Quit
+                <br/>
+                F10 : Save & Exit Setup
+            </div>
+
+            <div style={{ width: '50%'}}>
+                ↑ ↓ → ←   : Select Item
+            </div>
+        </wh>
+        <div className={styles.menuFooter2}>
+          Time, Date, hard Disk Type...
+        </div>
+    </div>
+  </div>
+);
 
 export default Bios;
+//
