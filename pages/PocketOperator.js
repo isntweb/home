bbcfd1cc8eb79cc8041be769e3b1ad0b17b5d6e0
Jacob Chvatal <jake@isnt.online>
2023-04-29 13:56:32 +0200

    knob abstraction

--- pages/PocketOperator.js
+++ pages/PocketOperator.js
@@ -4,6 +4,23 @@ const controlButtons = [
   'a', 'b', 'c', 'd', 'e',
 ]
 
+const Knob = ({ name }) => {
+  return (
+    <div className={classes.knob}>
+      <div className={classes.knobName}>{name}</div>
+      <div className={classes.knobValue}>0</div>
+    </div>
+  );
+}
+
+const Button = ({ name }) => {
+  return (
+    <div className={classes.button} >
+        {name}
+    </div>
+  );
+}
+
 const buttonGrid = [
   [1, 2, 3, 4],
   [5, 6, 7, 8],
@@ -15,20 +32,27 @@ const PocketOperator = () => {
   return (
     <div className={classes.container}>
       <div className={classes.body}>
+        <div className={classes.leftCutout}></div>
         <div className={classes.operatorText}>
           pocket operator
         </div>
+        <div className={classes.rightCutout}></div>
         <div className={classes.lcd}>
         </div>
         <div className={classes.buttonsTop}>
+          {controlButtons.map((name, index) => {
+            const Component = [4, 5].includes(index) ? Knob : Button;
+
+            return (
+              <Component name={name}/>
+            );
+          })}
         </div>
         <div className={classes.buttons}>
           {buttonGrid.map((row, i) =>
             <div className={classes.buttonRow}>
               {row.map((cell, i) => (
-                <div className={classes.button} key={i}>
-                  {cell}
-                </div>
+                <Button name={cell} key={i}/>
               ))}
             </div>
           )}
